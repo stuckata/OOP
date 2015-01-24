c9f4875b7086f5a1d13345e34330337d3e2c7f50
stuckata <stu.kirkov@gmail.com>
2015-01-24 10:50:43 +0200

    problem 03

---
+++ 01_DefiningClasses/Homework/01_DefiningClasses/03_PcCatalog/Component.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Component
+{
+    private string name;
+    private string details;
+    private decimal price;
+
+    public Component(string name, string details, decimal price)
+    {
+        this.Name = name;
+        this.Details = details;
+        this.Price = price;
+    }
+
+    public Component(string name, decimal price)
+    {
+        this.Name = name;
+        this.Price = price;
+    }
+
+    public string Name
+    {
+        get
+        {
+            return this.name;
+        }
+        set
+        {
+            Validators.AssertNotEmpty(value, "name");
+            this.name = value;
+        }
+    }
+
+    public string Details
+    {
+        get
+        {
+            return this.details;
+        }
+        set
+        {
+            Validators.AssertNotEmpty(value, "details");
+            this.details = value;
+        }
+    }
+
+    public decimal Price
+    {
+        get
+        {
+            return this.price;
+        }
+        set
+        {
+            Validators.AssertIsPositive(value, "price");
+            this.price = value;
+        }
+    }
+}
+
