e401e0aa04f8fa53e66f21600df25d0a93f6ff22
stuckata <stu.kirkov@gmail.com>
2015-01-26 17:31:04 +0200

    problem 02 solved

---
+++ 02_StaticMembersAndNamespaces/Homework/StaticMembersAndNamespaces/02_DistanceCalculator/DistanceCalculator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using _01_Point3D;
+
+namespace _02_DistanceCalculator
+{
+    public static class DistanceCalculator
+    {
+        public static double CalculateDistanceBetweenTwoPoints3D(Point3D point1, Point3D point2)
+        {
+            double result = Math.Sqrt(Math.Pow(point1.CoordinateX - point2.CoordinateX, 2) +
+                Math.Pow(point1.CoordinateY - point2.CoordinateY, 2) + 
+                Math.Pow(point1.CoordinateZ - point2.CoordinateZ, 2));
+            return result;
+        }
+    }
+}
---
+++ 02_StaticMembersAndNamespaces/Homework/StaticMembersAndNamespaces/02_DistanceCalculator/DistanceCalculatorTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using _01_Point3D;
+
+namespace _02_DistanceCalculator
+{
+    class DistanceCalculatorTest
+    {
+        static void Main()
+        {
+            Point3D point1 = new Point3D(3.48, 48.56, 8.746);
+            Point3D point2 = new Point3D(7, 14.5897, 164.2487);
+
+            double distanceBetweenPoints = DistanceCalculator.CalculateDistanceBetweenTwoPoints3D(point1, point2);
+
+            Console.WriteLine("Point1:");
+            Console.WriteLine(point1);
+            Console.WriteLine("Point2:");
+            Console.WriteLine(point2);
+            Console.WriteLine("Distance between them is: " + distanceBetweenPoints);
+        }
+    }
+}
