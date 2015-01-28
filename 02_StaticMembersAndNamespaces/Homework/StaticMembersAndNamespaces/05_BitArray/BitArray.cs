7da7816db069af8090b3c44032e7836cf9c380d3
stuckata <stu.kirkov@gmail.com>
2015-01-28 22:47:47 +0200

    problem 05 solved

---
+++ 02_StaticMembersAndNamespaces/Homework/StaticMembersAndNamespaces/05_BitArray/BitArray.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_BitArray
+{
+    class BitArray
+    {
+        public const int BITS_COUNT = 100000;
+        private uint bitValues;
+        private List<int> positionsOfBitsWithValueOne = new List<int>();
+
+        // Indexer declaration
+        public int this[int index]
+        {
+            get
+            {
+                if ((index >= 0) && (index < BITS_COUNT))
+                {
+                    // Check the bit at position index
+                    if ((bitValues & (1 << index)) == 0)
+                    {
+                        return 0;
+                    }
+                    else
+                    {
+                        return 1;
+                    }                
+                }
+                else
+                {
+                    throw new IndexOutOfRangeException(String.Format(
+                        "Index {0} is invalid!", index));
+                }
+            }
+
+            set
+            {
+                if ((index < 0) || (index > BITS_COUNT - 1))
+                {
+                    throw new IndexOutOfRangeException(String.Format(
+                        "Index {0} is invalid!", index));
+                }
+                if ((value < 0) || (value > 1))
+                {
+                    throw new ArgumentException(String.Format(
+                        "Value {0} is invalid!", value));
+                }
+
+                // Clear the bit at position index
+                bitValues &= ~((uint)(1 << index));
+
+                // Set the bit at position index to value
+                bitValues |= (uint)(value << index);
+                if (value != 0)
+                {
+                    this.positionsOfBitsWithValueOne.Add(index);
+                }    
+            }
+        }
+
+        public static long ConvertBinaryToDecimalNumber(List<int> positionsOfBitsWithValueOne)
+        {
+            long result = 0;
+
+            for (int i = 0; i < positionsOfBitsWithValueOne.Count(); i++)
+            {
+                result += (long)Math.Pow(2, positionsOfBitsWithValueOne[i]);
+            }
+            return result;
+        }
+
+        public override string ToString()
+        {
+            return ConvertBinaryToDecimalNumber(this.positionsOfBitsWithValueOne).ToString();
+        }
+    }
+}
--- 02_StaticMembersAndNamespaces/Homework/StaticMembersAndNamespaces/05_BitArray/BitArrayTest.cs
+++ 02_StaticMembersAndNamespaces/Homework/StaticMembersAndNamespaces/05_BitArray/BitArrayTest.cs
@@ -10,6 +10,15 @@ namespace _05_BitArray
     {
         static void Main(string[] args)
         {
+            BitArray bits = new BitArray();
+
+            bits[0] = 1;
+            bits[5] = 1;
+            bits[5] = 0;
+            bits[25] = 1;
+            bits[31] = 1;
+
+            Console.WriteLine("The decimal representation of this number is: " + bits);
         }
     }
 }
