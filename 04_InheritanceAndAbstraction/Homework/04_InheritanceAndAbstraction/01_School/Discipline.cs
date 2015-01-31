9fa02a6d242c8d8d227c06cf73d7ddd9c4e5def4
stuckata <stu.kirkov@gmail.com>
2015-01-31 17:14:53 +0200

    problem 01 solved

--- 04_InheritanceAndAbstraction/Homework/04_InheritanceAndAbstraction/01_School/Discipline.cs
+++ 04_InheritanceAndAbstraction/Homework/04_InheritanceAndAbstraction/01_School/Discipline.cs
@@ -6,7 +6,54 @@ using System.Threading.Tasks;
 
 namespace _01_School
 {
-    class Discipline
+    class Discipline : School
     {
+        private string name;
+        private int numberOfLectures;
+        private List<Student> students;
+
+        public Discipline(string details, string name, int numberOfLectures, List<Student> students)
+            : base(details)
+        {
+            this.Name = name;
+            this.NumberOfLectures = numberOfLectures;
+            this.Students = students;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+            set
+            {
+                this.name = value;
+            }
+        }
+
+        public int NumberOfLectures
+        {
+            get
+            {
+                return this.numberOfLectures;
+            }
+            set
+            {
+                this.numberOfLectures = value;
+            }
+        }
+
+        public List<Student> Students
+        {
+            get
+            {
+                return this.students;
+            }
+            set
+            {
+                this.students = value;
+            }
+        }
     }
 }
