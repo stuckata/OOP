12ac1d50b546d8e9c9e5b44d64c47e65ba10abed
stuckata <stu.kirkov@gmail.com>
2015-01-31 17:50:01 +0200

    problem 02 start solving

---
+++ 04_InheritanceAndAbstraction/Homework/04_InheritanceAndAbstraction/02_HumanStudentAndWorker/Student.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_HumanStudentAndWorker
+{
+    class Student : Human
+    {
+        private string faultyNumber;
+
+        public Student(string firstName, string lastName, string faultyNumber)
+            : base(firstName, lastName)
+        {
+            this.FaultyNumber = faultyNumber;
+        }
+
+        public string FaultyNumber
+        {
+            get
+            {
+                return this.faultyNumber;
+            }
+            set
+            {
+                this.faultyNumber = value;
+            }
+        }
+    }
+}
