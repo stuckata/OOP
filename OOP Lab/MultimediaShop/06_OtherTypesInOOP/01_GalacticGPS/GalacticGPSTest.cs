4ff14e465e92299375f795ed942dcb0ff056a62d
stuckata <stu.kirkov@gmail.com>
2015-02-10 11:09:09 +0200

    problem 01 solved

---
+++ OOP Lab/MultimediaShop/06_OtherTypesInOOP/01_GalacticGPS/GalacticGPSTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_GalacticGPS
+{
+    public class GalacticGPSTest
+    {
+        static void Main()
+        {
+            Location home = new Location(18.037986, 28.870097, Planet.Earth);
+            Console.WriteLine(home);
+            Location mars = new Location(25.046785, 54.870097, Planet.Mars);
+            Console.WriteLine(mars);
+        }
+    }
+}
---
+++ OOP Lab/MultimediaShop/06_OtherTypesInOOP/01_GalacticGPS/Location.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_GalacticGPS
+{
+    public struct Location
+    {
+        private double latitude;
+        private double longitude;
+        private Planet planet;
+
+        public Location(double latitude, double longitude, Planet planet)
+            : this()
+        {
+            this.Latitude = latitude;
+            this.Longitude = longitude;
+            this.Planet = planet;
+        }
+
+        public double Latitude
+        {
+            get
+            {
+                return this.latitude;
+            }
+            set
+            {
+                this.latitude = value;
+            }
+        }
+
+        public double Longitude
+        {
+            get
+            {
+                return this.longitude;
+            }
+            set
+            {
+                this.longitude = value;
+            }
+        }
+
+        public Planet Planet
+        {
+            get
+            {
+                return this.planet;
+            }
+            set
+            {
+                this.planet = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            return String.Format("Latitude: {0}, Longitude: {1} - Location: {2}", 
+                this.Latitude, this.Longitude, this.Planet);
+        }
+    }
+}
