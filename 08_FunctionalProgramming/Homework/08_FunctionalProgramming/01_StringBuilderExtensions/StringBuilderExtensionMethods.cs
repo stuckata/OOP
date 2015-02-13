f82a207481ebdf91147e8e48b18c4671ab136edc
stuckata <stu.kirkov@gmail.com>
2015-02-13 17:39:07 +0200

    08_FunctionalProgramming - problem 01 solved

---
+++ 08_FunctionalProgramming/Homework/08_FunctionalProgramming/01_StringBuilderExtensions/StringBuilderExtensionMethods.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace _01_StringBuilderExtensionMethods
+{
+    public static class StringBuilderExtensionMethods
+    {
+        public static string Substring(this StringBuilder stringBuilder,int startIndex, int length)
+        {
+            return stringBuilder.ToString(startIndex, length);
+        }
+
+        public static string RemoveText(this StringBuilder stringBuilder, string text)
+        {
+            return Regex.Replace(stringBuilder.ToString().Trim(), text, "", RegexOptions.IgnoreCase).Trim();
+        }
+
+        public static string AppendAll<T>(this StringBuilder stringBuilder, IEnumerable<T> items)
+        {
+            return string.Join(", ", items);
+        }
+    }
+}
---
+++ 08_FunctionalProgramming/Homework/08_FunctionalProgramming/01_StringBuilderExtensions/StringBuilderExtensionsTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using _01_StringBuilderExtensionMethods;
+
+namespace _01_StringBuilderExtensionsTest
+{
+    public class StringBuilderExtensionsTest
+    {
+        static void Main(string[] args)
+        {
+            string test = "The quick brown fox jumps over the lazy dog.";
+            string separator = "=============================================================================";
+            StringBuilder testText = new StringBuilder();
+            List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            Console.WriteLine("The test text -> " + test);
+            Console.WriteLine(separator);
+            testText.Append(test);
+
+            Console.WriteLine("Substring test -> " + testText.Substring(0, 9));
+            Console.WriteLine(separator);
+            Console.WriteLine("Remove test -> " + testText.RemoveText("FOX"));
+            Console.WriteLine(separator);
+            Console.WriteLine("AppendAll test with list of numbers [1...10]-> " + testText.AppendAll(numbers));
+            Console.WriteLine(separator);
+            Console.WriteLine();
+        }
+    }
+}
