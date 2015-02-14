aabcf8b1f17bb8038ed956ef6878e780c0903da2
stuckata <stu.kirkov@gmail.com>
2015-02-14 22:58:26 +0200

    problem 05 solved

---
+++ 08_FunctionalProgramming/Homework/08_FunctionalProgramming/05_StudentsByFirstAndLastName/StudentsByFirstAndLastName.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using _03_ClassStudent;
+
+namespace _05_StudentsByFirstAndLastName
+{
+    class StudentsByFirstAndLastName
+    {
+        static void Main(string[] args)
+        {
+            IList<Student> students = new List<Student>
+            {
+                new Student("Bilbo", "Baggins", 19, "123", "245686", "bilbo@shiremail.sh",
+                new List<int> { 3, 4, 5, 6 }, "2"),
+                new Student("Frodo", "Baggins", 21, "245", "684578", "frodo@underhill.sh",
+                new List<int> { 5, 5, 6, 6 }, "2"),
+                new Student("Gandolf", "Thegray", 20, "789", "486515", "gandolf@wizards.org",
+                new List<int> { 6, 6, 6, 6 }, "1"),
+                new Student("Sauron", "Unnamed", 26, "666", "123546", "sauron@mordormail.md",
+                new List<int> { 2, 3, 4, 2 }, "6"),
+                new Student("Elrond", "Earendil", 23, "007", "888888", "elrond@rivendell.me",
+                new List<int> { 2, 3, 4, 2 }, "3"),
+                new Student("Aragorn", "Arathorn", 22, "846", "435487", "theking@gondor.gr",
+                new List<int> { 6, 6, 5, 6 }, "3"),
+            };
+
+            string separator = "=========================";
+
+            Console.WriteLine("ALL STUDENTS LIST: ");
+            Console.WriteLine(separator);
+
+            foreach (var student in students)
+            {
+                Console.WriteLine(student);
+                Console.WriteLine();
+            }
+
+            IEnumerable<Student> studentQuery =
+                from student in students
+                where string.Compare(student.FirstName, student.LastName) == -1
+                orderby student.FirstName ascending
+                select student;
+
+            Console.WriteLine("STUDENTS BY FIRST AND LAST NAME: ");
+            Console.WriteLine(separator);
+
+            foreach (var student in studentQuery)
+            {
+                Console.WriteLine(student);
+                Console.WriteLine();
+            }
+        }
+    }
+}
