8a4f26c1ce39508c5a84281f9dcb2dabc936ebc6
stuckata <stu.kirkov@gmail.com>
2015-02-15 21:10:32 +0200

    problem 02 solved

--- 08_FunctionalProgramming/Homework/08_FunctionalProgramming/02_CustomLinqExtensionMethods/CustomLinqExtensionMethodsTest.cs
+++ 08_FunctionalProgramming/Homework/08_FunctionalProgramming/02_CustomLinqExtensionMethods/CustomLinqExtensionMethodsTest.cs
@@ -10,6 +10,43 @@ namespace _02_CustomLinqExtensionMethods
     {
         static void Main(string[] args)
         {
+            IList<int> numbers = new List<int>() { 1, 2, 3, 4, 5, 6, 7 };
+            var oddNumbers = numbers.WhereNot(n => n % 2 == 0);
+
+            Console.WriteLine("NUMBERS: ");
+            Console.WriteLine("[ {0} ]", String.Join(", ", numbers));
+            Console.WriteLine();
+            Console.WriteLine("ODD NUMBERS: ");
+            Console.WriteLine("[ {0} ]", String.Join(", ", oddNumbers));
+            Console.WriteLine();
+
+            var numbersToRepeat = numbers.Repeat(2);
+
+            Console.WriteLine("REPEAT NUMBERS: ");
+            Console.WriteLine("[ {0} ]", String.Join(", ", numbersToRepeat));
+            Console.WriteLine();
+
+            IList<string> words = new List<string>()
+            {
+                "cat",
+                "dog",
+                "robocat",
+                "robodog",
+                "caterpiller",
+                "shepherddog",
+                "turtle",
+                "rabit",
+                "crocodile"
+            };
+            IList<string> suffixes = new List<string>() { "cat", "dog"};
+            var wordsWithSuffix = words.WhereEndsWith(suffixes);
+
+            Console.WriteLine("WORDS: ");
+            Console.WriteLine("[ {0} ]", String.Join(", ", words));
+            Console.WriteLine();
+            Console.WriteLine("WORDS WHICH END WITH SUFIXES 'cat' AND 'dog': ");
+            Console.WriteLine("[ {0} ]", String.Join(", ", wordsWithSuffix));
+            Console.WriteLine();
         }
     }
 }
--- 08_FunctionalProgramming/Homework/08_FunctionalProgramming/02_CustomLinqExtensionMethods/LinqExtensionMethods.cs
+++ 08_FunctionalProgramming/Homework/08_FunctionalProgramming/02_CustomLinqExtensionMethods/LinqExtensionMethods.cs
@@ -10,7 +10,39 @@ namespace _02_CustomLinqExtensionMethods
     {
         public static IEnumerable<T> WhereNot<T>(this IEnumerable<T> collection, Func<T, bool> predicate)
         {
-            var result = 
+            foreach (var item in collection)
+            {
+                if (!predicate(item))
+                {
+                    yield return item;
+                }
+            }
+        }
+
+        public static IEnumerable<T> Repeat<T>(this IEnumerable<T> collection, int count)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                foreach (var item in collection)
+                {
+                    yield return item;
+                }
+            }
+        }
+
+        public static IEnumerable<string> WhereEndsWith(this IEnumerable<string> collection, 
+            IEnumerable<string> suffixes)
+        {
+            foreach (var word in collection)
+            {
+                foreach (var suffix in suffixes)
+                {
+                    if (word.ToString().Trim().EndsWith(suffix))
+                    {
+                        yield return word.Trim();
+                    }
+                }
+            }
         }
     }
 }
