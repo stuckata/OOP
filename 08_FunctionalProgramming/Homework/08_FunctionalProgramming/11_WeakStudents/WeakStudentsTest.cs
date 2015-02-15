c2cd2031b9eb1961ffb2c99db75d68bb21e1b1d8
stuckata <stu.kirkov@gmail.com>
2015-02-15 13:01:07 +0200

    problem 11 started

---
+++ 08_FunctionalProgramming/Homework/08_FunctionalProgramming/11_WeakStudents/WeakStudentsTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using _03_ClassStudent;
+
+namespace _11_WeakStudents
+{
+    public class WeakStudentsTest
+    {
+        static void Main(string[] args)
+        {
+            IList<Student> students = StudentsList.GetStudents();
+
+            IEnumerable<Student> studentsQuery =
+                from student in students
+                where student.Marks.Contains(new List<int> { 2, 2 })
+                select student;
+
+            Console.WriteLine("STUDENTS WITH AT LEAST ONE EXCELLENT MARK (6): ");
+            Console.WriteLine();
+
+            foreach (var student in studentsQuery)
+            {
+                Console.WriteLine(student);
+                Console.WriteLine();
+            }
+        }
+    }
+}
