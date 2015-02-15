3416d0ec67641921f8f1845528882166c3eed62f
stuckata <stu.kirkov@gmail.com>
2015-02-15 19:29:50 +0200

    problem 02 started

---
+++ 08_FunctionalProgramming/Homework/08_FunctionalProgramming/02_CustomLinqExtensionMethods/LinqExtensionMethods.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_CustomLinqExtensionMethods
+{
+    public static class LinqExtensionMethods
+    {
+        public static IEnumerable<T> WhereNot<T>(this IEnumerable<T> collection, Func<T, bool> predicate)
+        {
+            var result = 
+        }
+    }
+}
