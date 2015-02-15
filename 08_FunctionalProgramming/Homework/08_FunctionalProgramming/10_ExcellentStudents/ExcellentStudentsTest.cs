b7010e0d60d93a879bfd8f488d8de4e048b5f4f1
stuckata <stu.kirkov@gmail.com>
2015-02-15 12:50:36 +0200

    problem 10 solved

---
+++ 08_FunctionalProgramming/Homework/08_FunctionalProgramming/10_ExcellentStudents/ExcellentStudentsTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using _03_ClassStudent;
+
+namespace _10_ExcellentStudents
+{
+    public class ExcellentStudentsTest
+    {
+        static void Main(string[] args)
+        {
+            IList<Student> students = StudentsList.GetStudents();
+
+            IEnumerable<Student> studentsQuery =
+                from student in students
+                where student.Marks.Contains(6)
+                select student;
+
+            Console.WriteLine("STUDENTS WITH AT LEAST ONE EXCELLENT MARK (6): ");
+            Console.WriteLine();
+
+            foreach (var student in studentsQuery)
+            {
+                Console.WriteLine(student);
+                Console.WriteLine();
+            }
+        }
+    }
+}
