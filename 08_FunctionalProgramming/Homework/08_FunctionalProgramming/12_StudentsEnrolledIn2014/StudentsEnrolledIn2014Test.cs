3ca04f88eb0976016422877b551c43acfd9e81ec
stuckata <stu.kirkov@gmail.com>
2015-02-15 17:54:25 +0200

    problem 12 solved

---
+++ 08_FunctionalProgramming/Homework/08_FunctionalProgramming/12_StudentsEnrolledIn2014/StudentsEnrolledIn2014Test.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using _03_ClassStudent;
+
+namespace _12_StudentsEnrolledIn2014
+{
+    class StudentsEnrolledIn2014Test
+    {
+        static void Main(string[] args)
+        {
+            IList<Student> students = StudentsList.GetStudents();
+
+            IEnumerable<Student> studentsQuery =
+                from student in students
+                where student.FacultyNumber.IndexOf("4") == 5
+                select student;
+
+            Console.WriteLine("STUDENTS ENROLLED IN 2014: ");
+            Console.WriteLine();
+
+            foreach (var student in studentsQuery)
+            {
+                Console.WriteLine(student);
+                Console.WriteLine();
+            }
+        }
+    }
+}
