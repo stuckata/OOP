046233249c0bef3a80eb198ff51ff4980a9a420f
stuckata <stu.kirkov@gmail.com>
2015-02-17 13:45:41 +0200

    problem 01 skeleton ready

---
+++ 09_CommonTypeSystem/Homework/09_CommonTypeSystem/01_Customer/Customer.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_Customer
+{
+    public class Customer : ICloneable, IComparable<Customer>
+    {
+        private string firstName;
+        private string middleName;
+        private string lastName;
+        private string id;
+        private string permanetAddress;
+        private string phoneNumber;
+        private string email;
+        private List<Payment> payments;
+        private CustomerType type;
+
+        public Customer(string firstName, string middleName, string lastName, string id, string permanetAddress,
+            string phoneNumber, string email, List<Payment> payments, CustomerType type)
+        {
+            this.FirstName = firstName;
+            this.MiddleName = middleName;
+            this.LastName = lastName;
+            this.Id = id;
+            this.PermanetAddress = permanetAddress;
+            this.PhoneNumber = phoneNumber;
+            this.Email = email;
+            this.Payments = payments;
+            this.Type = type;
+        }
+
+        public string FirstName
+        {
+            get
+            {
+                return this.firstName;
+            }
+            set
+            {
+                this.firstName = value;
+            }
+        }
+
+        public string MiddleName
+        {
+            get
+            {
+                return this.middleName;
+            }
+            set
+            {
+                this.middleName = value;
+            }
+        }
+
+        public string LastName
+        {
+            get
+            {
+                return this.lastName;
+            }
+            set
+            {
+                this.lastName = value;
+            }
+        }
+
+        public string Id
+        {
+            get
+            {
+                return this.id;
+            }
+            set
+            {
+                this.id = value;
+            }
+        }
+
+        public string PermanetAddress
+        {
+            get
+            {
+                return this.permanetAddress;
+            }
+            set
+            {
+                this.permanetAddress = value;
+            }
+        }
+
+        public string PhoneNumber
+        {
+            get
+            {
+                return this.phoneNumber;
+            }
+            set
+            {
+                this.phoneNumber = value;
+            }
+        }
+
+        public string Email
+        {
+            get
+            {
+                return this.email;
+            }
+            set
+            {
+                this.email = value;
+            }
+        }
+
+        public List<Payment> Payments
+        {
+            get
+            {
+                return this.payments;
+            }
+            set
+            {
+                this.payments = value;
+            }
+        }
+
+        public CustomerType Type
+        {
+            get
+            {
+                return this.type;
+            }
+            set
+            {
+                this.type = value;
+            }
+        }
+
+        public object Clone()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int CompareTo(Customer other)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ 09_CommonTypeSystem/Homework/09_CommonTypeSystem/01_Customer/Payment.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_Customer
+{
+    public class Payment
+    {
+        private string productName;
+        private decimal price;
+
+        public Payment(string productName, decimal price)
+        {
+            this.ProductName = productName;
+            this.Price = price;
+        }
+
+        public string ProductName
+        {
+            get
+            {
+                return this.productName;
+            }
+            set
+            {
+                this.productName = value;
+            }
+        }
+
+        public decimal Price
+        {
+            get
+            {
+                return this.price;
+            }
+            set
+            {
+                this.price = value;
+            }
+        }
+    }
+}
