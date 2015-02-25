c2e70f53178e17304c69c1c488bcf23bedcde5e2
stuckata <stu.kirkov@gmail.com>
2015-02-25 15:48:47 +0200

    SampleExam24Oct2014 added and start resolving

---
+++ SampleExam24Oct2014/01_Estates/Estates-Skeleton/Data/EstateFactory.cs
@@ -0,0 +1,24 @@
+﻿using Estates.Engine;
+using Estates.Interfaces;
+using System;
+
+namespace Estates.Data
+{
+    public class EstateFactory
+    {
+        public static IEstateEngine CreateEstateEngine()
+        {
+            return new EstateEngine();
+        }
+
+        public static IEstate CreateEstate(EstateType type, string name, decimal area, string location, bool isFurnitured, int numberOfRooms, bool hasElevator)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static IOffer CreateOffer(OfferType type)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
