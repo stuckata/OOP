98fcfac90c0c555a0a4876d750a53a14e8753560
stuckata <stu.kirkov@gmail.com>
2015-02-25 21:01:00 +0200

    Garage and House classes added

--- SampleExam24Oct2014/01_Estates/Estates-Skeleton/Data/Garage.cs
+++ SampleExam24Oct2014/01_Estates/Estates-Skeleton/Data/Garage.cs
@@ -3,10 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Estates.Interfaces;
 
 namespace Estates.Data
 {
-    public class Garage
+    public class Garage : Estate, IGarage
     {
+        private int width;
+        private int height;
+
+        public Garage(EstateType type, string name, double area, string location, bool isFurnished, int width, int height)
+            : base(type, name, area, location, isFurnished)
+        {
+            this.Width = width;
+            this.Height = height;
+        }
+
+        public int Width
+        {
+            get
+            {
+                return this.width;
+            }
+
+            set
+            {
+                this.width = value;
+            }
+        }
+
+        public int Height
+        {
+            get
+            {
+                return this.height;
+            }
+
+            set
+            {
+                this.height = value;
+            }
+        }
     }
 }
--- SampleExam24Oct2014/01_Estates/Estates-Skeleton/Data/House.cs
+++ SampleExam24Oct2014/01_Estates/Estates-Skeleton/Data/House.cs
@@ -3,10 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Estates.Interfaces;
 
 namespace Estates.Data
 {
-    public class House
+    public class House : Estate, IHouse
     {
+        private int floors;
+
+        public House(EstateType type, string name, double area, string location, bool isFurnished, int floors)
+            : base(type, name, area, location, isFurnished)
+        {
+            this.Floors = floors;
+        }
+
+        public int Floors
+        {
+            get
+            {
+                return this.floors;
+            }
+
+            set
+            {
+                this.floors = value;
+            }
+        }
     }
 }
