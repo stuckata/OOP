552a943d6f23be28345295225b1e4c6dd2c78028
stuckata <stu.kirkov@gmail.com>
2015-02-25 18:14:36 +0200

    Apartment and Office classes ready

--- SampleExam24Oct2014/01_Estates/Estates-Skeleton/Data/Apartment.cs
+++ SampleExam24Oct2014/01_Estates/Estates-Skeleton/Data/Apartment.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Estates.Interfaces;
 
 namespace Estates.Data
 {
-    public class Apartment
+    public class Apartment : BuildingEstate, IApartment
     {
+        public Apartment(EstateType type, string name, double area, string location, bool isFurnished, int rooms, bool hasElevator)
+            : base(type, name, area, location, isFurnished, rooms, hasElevator)
+        {                
+        }
     }
 }
--- SampleExam24Oct2014/01_Estates/Estates-Skeleton/Data/Office.cs
+++ SampleExam24Oct2014/01_Estates/Estates-Skeleton/Data/Office.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Estates.Interfaces;
 
 namespace Estates.Data
 {
-    public class Office
+    public class Office : BuildingEstate, IOffice
     {
+        public Office(EstateType type, string name, double area, string location, bool isFurnished, int rooms, bool hasElevator)
+            : base(type, name, area, location, isFurnished, rooms, hasElevator)
+        {
+        }
     }
 }
