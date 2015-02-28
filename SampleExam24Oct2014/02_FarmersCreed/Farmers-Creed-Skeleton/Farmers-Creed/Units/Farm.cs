07bfbed4828bdbf5fc1d03e6760f4716411b19bb
stuckata <stu.kirkov@gmail.com>
2015-02-28 10:53:00 +0200

    farmer's creed start resolving

---
+++ SampleExam24Oct2014/02_FarmersCreed/Farmers-Creed-Skeleton/Farmers-Creed/Units/Farm.cs
@@ -0,0 +1,59 @@
+﻿namespace FarmersCreed.Units
+{
+    using System;
+    using System.Collections.Generic;
+    using FarmersCreed.Interfaces;
+
+    public class Farm : GameObject, IFarm
+    {
+        private List<Plant> plants;
+        private List<Animal> animals;
+        private List<Product> products;
+
+        public Farm(string id)
+            : base(id)
+        {
+        }
+
+        public List<Plant> Plants
+        {
+            get { return this.plants; }
+        }
+
+        public List<Animal> Animals
+        {
+            get { return this.animals; }
+        }
+
+        public List<Product> Products
+        {
+            get { return this.products; }
+        }
+
+        public void AddProduct(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Exploit(IProductProduceable productProducer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Feed(Animal animal, IEdible edibleProduct, int productQuantity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Water(Plant plant)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateFarmState()
+        {
+            // TODO: Process all animal and plant behavior
+            throw new NotImplementedException();
+        }
+    }
+}
